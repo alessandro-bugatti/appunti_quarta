d7368345c5835f5eb13bf1fa4c80c5a47d0df0e0
alessandro-bugatti <alessandro.bugatti@gmail.com>
2020-09-17 16:49:47 +0200

    Aggiunge un primo esempio con una classe vuota

---
+++ primi_esempi/vettori_esempio/src/vettori_esempio/Vettori_esempio.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vettori_esempio;
+
+/**
+ *
+ * @author alex_2
+ */
+public class Vettori_esempio {
+  
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Vettore v1, v2;
+        v1 = new Vettore();
+        v2 = new Vettore();
+        System.out.println("Hello Alessandro!");
+        
+    }
+    
+}
