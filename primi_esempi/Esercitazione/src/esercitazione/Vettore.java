e3b9bb191a340bf0b6118bff0fc98864c462b241
alessandro-bugatti <alessandro.bugatti@gmail.com>
2020-10-14 12:39:30 +0200

    Aggiorna l'esercitazione

--- primi_esempi/Esercitazione/src/esercitazione/Vettore.java
+++ primi_esempi/Esercitazione/src/esercitazione/Vettore.java
@@ -12,25 +12,34 @@ public class Vettore
     //attributi
     private int dimensione;
     private int v[];
+    private int merged[];
     
     //Costruttore che crea un vettore di dim elementi 
     //causali positivi
     public Vettore(int dim)
     { 
-        
+        dimensione = dim;
+        v =  new int[dimensione];
+        for (int i = 0; i < dimensione; i++)
+            v[i] = (int)(Math.random()*100) + 1;
     }
     
     //Fa la somma di tutti gli elementi del vettore
     public int somma()
     {
-        return 42;
+        int temp = 0;
+        for (int i = 0; i < dimensione;i++)
+            temp += v[i];
+        return temp;
     }
     
     //Ritorna il valore dell'elemento in posizione pos
     //Se pos è un indice non valido ritorna -1
     public int get(int pos)
     {
-        return 42;
+        if (pos < 0 || pos >= dimensione)
+            return -1;
+        return v[pos];
     }
     
     //Setta il valore dell'elemento in posizione 
@@ -38,14 +47,20 @@ public class Vettore
     //la funzione ritorna false, true altrimenti
     public boolean set(int n, int pos)
     {
+        if (pos < 0 || pos >= dimensione)
+            return false;
+        v[pos] = n;
         return true;
     }
     
     //Ricerca l'elemento cercato all'interno del vettore
     //Se lo trova ritorna la posizione, altrimenti -1
-    public int ricerca_esaustiva(int cercato)
+    public int ricercaEsaustiva(int cercato)
     {
-        return 42;
+        for (int i = 0; i < this.dimensione; i++)
+            if (v[i] == cercato)
+                return i;
+        return -1;
     }
     
     /*
@@ -63,32 +78,91 @@ public class Vettore
     
     public void selectionSort()
     {
-        
+        selectionSort(0, this.dimensione);
     }
     
     public void selectionSort(int a, int b)
     {
-        
+        if (a > b)
+            return;
+        if (a < 0)
+            a = 0;
+        if (b > this.dimensione)
+            b = this.dimensione;
+        int index = a;
+        while (index < b)
+        {
+            int minore = v[index];
+            int pos = index;
+            for (int i = index; i < b; i++)
+                if (v[i] < minore)
+                {
+                    minore = v[i];
+                    pos = i;
+                }    
+            int temp = v[index];
+            v[index] = v[pos];
+            v[pos] = temp;
+            index++;
+        }
     }
     
     public void bubbleSort()
     {
-        
+        bubbleSort(0, this.dimensione);
     }
      
     public void bubbleSort(int a, int b)
     {
-        
+        if (a > b)
+            return;
+        if (a < 0)
+            a = 0;
+        if (b > this.dimensione)
+            b = this.dimensione;
+        boolean swapped;
+        int index = a;
+        do
+        {
+            swapped = false;
+            for (int j = b - 1; j > index; j--)
+            {
+                if (v[j] < v[j - 1])
+                {
+                    int temp = v[j];
+                    v[j] = v[j - 1];
+                    v[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+            index++;
+        }while(swapped);
     }
     
     public void insertionSort()
     {
-        
+        insertionSort(0, this.dimensione);
     }
     
     public void insertionSort(int a , int b)
     {
-        
+        if (a > b)
+            return;
+        if (a < 1)
+            a = 0;
+        if (b > this.dimensione)
+            b = this.dimensione;
+        for (int i = a; i < b; i++)
+        {
+            int index = i;
+            while (index - 1 >= a && v[index] < v[index - 1])
+            {
+                int temp = v[index];
+                v[index] = v[index - 1];
+                v[index - 1] = temp;
+                index--;
+            }
+        }
     }
     
     //Di questi due metodi bisogna fare solo la versione
@@ -96,30 +170,98 @@ public class Vettore
     //e il merging vanno chiamate come si vuole e dichiarate
     //private
     
+    private int partition(int a, int b)
+    {
+        int i = a - 1;
+        int j = b;
+        int pivot = v[b];
+        while(true)
+        {
+            i++;
+            j--;
+            while(v[i] < pivot) 
+                i++;
+            while( v[j] > pivot && j > a) 
+                j--;
+            if (i >= j)
+                break;
+            int temp = v[i];
+            v[i] = v[j];
+            v[j] = temp;
+        }
+        int temp = v[i];
+        v[i] = v[b];
+        v[b] = temp;
+        return i;
+    }
+    
     private void quickSortR(int a, int b)
     {
-    
+        if (a >= b)
+            return;
+        int i = partition(a, b);
+        quickSortR(a, i-1);
+        quickSortR(i+1, b);
     }
     
     public void quickSort()
     {
-        quickSortR(0,dimensione);
+        quickSortR(0,dimensione-1);
+    }
+    
+    private void merge(int a, int half, int b)
+    {
+        int i = a;
+        int j = half + 1;
+        int k = a;
+        for (;;) //while (i <= half && j <= b)
+        {
+            if (v[i] < v[j])
+            {
+                merged[k] = v[i];
+                i++;
+            }
+            else
+            {
+                merged[k] = v[j];
+                j++;
+            }
+            k++;
+            if (i > half || j > b)
+                break;
+        }
+        for (;i <= half; i++, k++)
+            merged[k] = v[i];
+        for (;j <= b; j++, k++)
+            merged[k] = v[j];
+        for (i = a; i <= b; i++)
+            v[i] = merged[i];
     }
     
     private void mergeSortR(int a, int b)
     {
-    
+        if (b - a <= 0)
+            return;
+        int half = (b + a)/2;
+        mergeSortR(a, half);
+        mergeSortR(half + 1, b);
+        merge(a, half, b);
     }
-    
+    // {5,3,2,8,4}
     public void mergeSort()
     {
-        mergeSortR(0,dimensione);
+        merged = new int[dimensione];
+        mergeSortR(0, dimensione - 1);
     }
 
     //Restituisce il valore minino del vettore
     public int minimo()
     {
-        return 42;
+        int minimo = v[0];
+        for (int i = 1; i < this.dimensione; i++)
+            if (v[i] < minimo)
+                minimo = v[i];
+        return minimo;
     }
     
     //Questa la tenete così com'è
---
+++ primi_esempi/Esercitazione/test/esercitazione/VettoreTest.java
@@ -0,0 +1,234 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esercitazione;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author alex_2
+ */
+public class VettoreTest {
+    
+    private Vettore instance;
+    
+    public VettoreTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        instance = new Vettore(5);
+        instance.set(5, 0);
+        instance.set(3, 1);
+        instance.set(2, 2);
+        instance.set(8, 3);
+        instance.set(4, 4);
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    /**
+     * Test of somma method, of class Vettore.
+     */
+    @Test
+    public void testSomma() {
+        System.out.println("somma");
+        int expResult = 22;
+        int result = instance.somma();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of get method, of class Vettore.
+     */
+    @Test
+    public void testGet() {
+        System.out.println("get");
+        int pos = 3;
+        int expResult = 2;
+        int result = instance.get(pos);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testGetTooLow() {
+        System.out.println("get too low");
+        int pos = -3;
+        int expResult = -1;
+        int result = instance.get(pos);
+        assertEquals(expResult, result);
+    }
+    
+    @Test
+    public void testGetTooHigh() {
+        System.out.println("get too high");
+        int pos = 10;
+        int expResult = -1;
+        int result = instance.get(pos);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of set method, of class Vettore.
+     */
+    @Test
+    public void testSet() {
+        System.out.println("set");
+        int n = 100;
+        int pos = 3;
+        int expResult = 100;
+        instance.set(n, pos);
+        int result = instance.get(pos);
+        assertEquals(expResult, result);
+    }
+        
+    @Test
+    public void testSetInvalid() {
+        System.out.println("set invalid");
+        int n = 100;
+        int pos = 30;
+        boolean expResult = false;
+        boolean result = instance.set(n, pos);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of ricerca_esaustiva method, of class Vettore.
+     */
+    @Test
+    public void testRicerca_esaustiva() {
+        System.out.println("ricerca_esaustiva");
+        int cercato = 8;
+        int expResult = 0;
+        int result = instance.ricerca_esaustiva(cercato);
+        assertEquals(expResult, result);
+        }
+
+    /**
+     * Test of ricerca_esaustiva method, of class Vettore.
+     */
+    @Test
+    public void testRicerca_esaustivaFailed() {
+        System.out.println("ricerca_esaustiva failed");
+        int cercato = 17;
+        int expResult = -1;
+        int result = instance.ricerca_esaustiva(cercato);
+        assertEquals(expResult, result);
+        }
+
+    
+    /**
+     * Test of selectionSort method, of class Vettore.
+     */
+    @Test
+    public void testSelectionSort_0args() {
+        System.out.println("selectionSort");
+        String expResult = "2 - 3 - 4 - 5 - 8";
+        instance.selectionSort();
+        assertEquals(expResult, instance.toString());
+    }
+
+    @Test
+    public void testSelectionSort_int_int() {
+        System.out.println("selectionSort interval");
+        int a = 1;
+        int b = 3;
+        instance.selectionSort(a, b);
+        String expResult = "5 - 2 - 3 - 8 - 4";
+        assertEquals(expResult, instance.toString());
+    }
+    
+    @Test
+    public void testSelectionSort_int_int_invalid() {
+        System.out.println("selectionSort invalid interval");
+        int a = -7;
+        int b = 20;
+        instance.selectionSort(a, b);
+        String expResult = "2 - 3 - 4 - 5 - 8";
+        assertEquals(expResult, instance.toString());
+    }
+
+    /**
+     * Test of bubbleSort method, of class Vettore.
+     */
+    @Test
+    public void testBubbleSort_0args() {
+        System.out.println("bubbleSort");
+        String expResult = "2 - 3 - 4 - 5 - 8";
+        instance.bubbleSort();
+        assertEquals(expResult, instance.toString());
+    }
+
+    /**
+     * Test of insertionSort method, of class Vettore.
+     */
+    @Test
+    public void testInsertionSort_0args() {
+        System.out.println("insertionSort");
+        String expResult = "2 - 3 - 4 - 5 - 8";
+        instance.bubbleSort();
+        assertEquals(expResult, instance.toString());
+    }
+
+    /**
+     * Test of quickSort method, of class Vettore.
+     */
+    @Test
+    public void testQuickSort() {
+        System.out.println("quickSort");
+        String expResult = "2 - 3 - 4 - 5 - 8";
+        instance.bubbleSort();
+        assertEquals(expResult, instance.toString());
+    }
+
+    /**
+     * Test of mergeSort method, of class Vettore.
+     */
+    @Test
+    public void testMergeSort() {
+        System.out.println("mergeSort");
+        String expResult = "2 - 3 - 4 - 5 - 8";
+        instance.bubbleSort();
+        assertEquals(expResult, instance.toString());
+    }
+
+    /**
+     * Test of minimo method, of class Vettore.
+     */
+    @Test
+    public void testMinimo() {
+        System.out.println("minimo");
+        int expResult = 2;
+        int result = instance.minimo();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of toString method, of class Vettore.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        String expResult = "5 - 3 - 2 - 8 - 4";
+        assertEquals(expResult, instance.toString());
+    }
+    
+}
