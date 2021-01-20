fd828c5e54e4b20b2203a5c7ab71a5c00772fbcb
alessandro-bugatti <alessandro.bugatti@gmail.com>
2021-01-20 11:48:10 +0100

    Ripresa del primo esempio con video associato

---
+++ javafx/javafx_button/src/javafx_button/FXMLDocumentController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafx_button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author alex_2
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private Button btnCliccami;
+    @FXML
+    private Label lblHelloWorld;
+    
+       
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void handleBtnCliccami(ActionEvent event) {
+        System.out.println("Sono stato cliccato");
+        lblHelloWorld.setText("Ciao mondo");
+    
+    }
+    
+}
