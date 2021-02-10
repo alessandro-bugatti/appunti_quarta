a331e72b2060bdce927cc8ae05cbbc4ad5c41634
alessandro-bugatti <alessandro.bugatti@gmail.com>
2021-02-10 10:52:31 +0100

    Aggiunta la gestione dello zucchero

--- javafx/javafx_coffee_machine/src/javafx_coffee_machine/FXMLDocumentController.java
+++ javafx/javafx_coffee_machine/src/javafx_coffee_machine/FXMLDocumentController.java
@@ -10,6 +10,7 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Slider;
@@ -34,11 +35,43 @@ public class FXMLDocumentController implements Initializable {
     private ToggleGroup grpBevande;
     @FXML
     private Slider sldZucchero;
+    @FXML
+    private Label lblZucchero;
+    @FXML
+    private Button btnEroga;
+    
+    private String etichetteZucchero[];
     
       
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        etichetteZucchero = new String[5];
+        etichetteZucchero[0] = "Niente zucchero";
+        etichetteZucchero[1] = "Poco zucchero";
+        etichetteZucchero[2] = "Zucchero normale";
+        etichetteZucchero[3] = "Molto zucchero";
+        etichetteZucchero[4] = "Zucchero a palate";
+        //Aggiunge un listener che è una lambda function
+        sldZucchero.valueProperty().addListener((observable, oldValue, newValue)
+        -> {
+            //Soluzione uno
+            /*if (newValue.doubleValue() == 0.0)
+                lblZucchero.setText("Niente zucchero");
+            else if (newValue.doubleValue() == 1.0)
+                lblZucchero.setText("Poco zucchero");
+            else if (newValue.doubleValue() == 2.0)
+                lblZucchero.setText("Zucchero normale");
+            else if (newValue.doubleValue() == 3.0)
+                lblZucchero.setText("Molto zucchero");
+            else 
+                lblZucchero.setText("Zucchero a palate");
+            */
+            lblZucchero.setText(etichetteZucchero[newValue.intValue()]);
+            
+            
+        }
+        
+        );
     }    
 
     @FXML
@@ -60,5 +93,10 @@ public class FXMLDocumentController implements Initializable {
         lblPrezzo.setText("Prezzo:" + this.prezzo + " dollari.");
         System.out.println(sldZucchero.getValue());
         
+    }
+
+    @FXML
+    private void handleBtnEroga(ActionEvent event) {
+        System.out.println("Zucchero " + etichetteZucchero[(int)sldZucchero.getValue()]);
     }
 }
