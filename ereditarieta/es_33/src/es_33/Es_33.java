b251b8362528c87d7abd64bc75b722add168e56b
alessandro-bugatti <alessandro.bugatti@gmail.com>
2021-03-03 10:03:55 +0100

    Revert "Aggiunto il polimorfismo"
    This reverts commit 9f928c646164fa4cc27571dcdd0420216c541e32.

--- ereditarieta/es_33/src/es_33/Es_33.java
+++ ereditarieta/es_33/src/es_33/Es_33.java
@@ -15,39 +15,18 @@ public class Es_33 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Abitazione abitazione = new Abitazione(4,75,"Via dei Pini 12", "Brescia");
+        Abitazione a = new Abitazione(4,75,"Via dei Pini 12", "Brescia");
         //System.out.println(a);
-        Villa villa = new Villa(2,1000,true,7, 200, "Via dei Ciliegi 23", "Brescia");
+        Villa b = new Villa(2,1000,true,7, 200, "Via dei Ciliegi 23", "Brescia");
         //Up-casting legale
-        Abitazione abitazione2 = new Villa(3,10000,true,17, 300, "Via dei Ciliegi 24", "Brescia");
-        villa.setStanze(4);
+        Abitazione c = new Villa(3,10000,true,17, 300, "Via dei Ciliegi 24", "Brescia");
+        b.setStanze(4);
         //Down-casting illegale, molto spesso lo è
         //Villa d = new Abitazione(3,55,"Via dei Pini 22", "Brescia");
-        abitazione2.setCitta("Milano");
-        Villa villa2;
-        
-        villa2 = (Villa)abitazione2;//Down-casting legale
-        villa2.getGiardino(); //Si può fare
-        System.out.println(villa2);
-        //villa2 = (Villa)abitazione; //Down-casting illegale
-        //villa2.getGiardino(); //Non si può fare
-        System.out.println(villa2);
-        //Up-casting: sempre legale
-        abitazione2 = villa; //Up-casting implicito
-        abitazione2 = (Abitazione)villa; //Up-casting esplicito
-        abitazione2.setCitta("Milano");
-        
-        Abitazione abitazioni[] = new Abitazione[4];
-        System.out.println(abitazioni[0]);
-        abitazioni[0] = abitazione;
-        abitazioni[1] = abitazione2;
-        abitazioni[2] = villa;
-        abitazioni[3] = villa2;
-        
-        
-        for (int i = 0; i < abitazioni.length; i++)
-            System.out.println("Abitazione " + i + ": " + abitazioni[i]);
-        
+        c.setCitta("Milano");
+        Villa d = (Villa)c;
+        //d.getGiardino();
+        System.out.println(c);
     }
     
 }
