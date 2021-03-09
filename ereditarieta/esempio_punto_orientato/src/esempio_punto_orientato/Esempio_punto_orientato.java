b2cc22ae2c1d4993c321b4ff01145cf59bab0b41
alessandro-bugatti <alessandro.bugatti@gmail.com>
2021-03-09 15:38:12 +0100

    Aggiunge esempi di up e down casting

--- ereditarieta/esempio_punto_orientato/src/esempio_punto_orientato/Esempio_punto_orientato.java
+++ ereditarieta/esempio_punto_orientato/src/esempio_punto_orientato/Esempio_punto_orientato.java
@@ -15,6 +15,8 @@ public class Esempio_punto_orientato {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Punto p = new Punto(9.3, 8.7);
+        
         PuntoOrientato po = new PuntoOrientato(9, 6, 'D');
         System.out.println(po);
         PuntoOrientato po2 = new PuntoOrientato(po);
@@ -29,6 +31,30 @@ public class Esempio_punto_orientato {
         else
             System.out.println("Diversi");
         
+        Punto padre;
+        PuntoOrientato figlio;
+        //Nè upcasting nè downcasting;
+        padre = p;
+        //L'upcasting è sempre valido, legale
+        //Upcasting implicito
+        padre = po;
+        //Upcasting esplicito
+        padre = (Punto)po;
+        //Il downcasting non è sempre legale
+        //Il downcasting deve essere esplicito
+        //In questo caso non sarebbe legale,
+        //e ce ne si accorge in runtime
+        //Downcasting legale
+        figlio = (PuntoOrientato)padre;
+        figlio.sposta(0);
+        //Downcasting illegale in runtime
+        figlio = (PuntoOrientato)p;
+        figlio.sposta(0);
+        
+        //Downcasting che torna a essere legale
+        p =(Punto)po;
+        figlio = (PuntoOrientato)p;
+        figlio.sposta(0);
     }
     
 }
