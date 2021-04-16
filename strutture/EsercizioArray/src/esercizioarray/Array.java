da0cc91aaa43316118797aafd9b2db72771ebe01
alessandro-bugatti <alessandro.bugatti@gmail.com>
2021-04-16 12:43:53 +0200

    Primo esempio di classe array che mima un ArrayList, da completare

---
+++ strutture/EsercizioArray/src/esercizioarray/Array.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package esercizioarray;
+
+/**
+ *
+ * @author Alessandro Bugatti
+ */
+
+//Rappresenta un vettore di interi
+public class Array {
+    
+    
+    
+    public Array()
+    {
+        
+    }
+    
+    public boolean add(int i, int e)
+    {
+        
+    }
+    
+    //Aggiunge un elemento in coda al vettore, 
+    //deve essere sempre possibile
+    public boolean add(int e)
+    {
+        
+    }
+    
+    //Array a che contiene [2,3,4]
+    //Array b che contiene [9,8,7]
+    //a.addAll(1, b) => a conterrà [2, 9, 8, 7, 3, 4]
+    //Anche questo dee essere sempre possibile
+    //se l'indice è corretto
+    public boolean addAll(int i, Array a)
+    {
+        
+    }
+    
+    //Equivalente di a[i];
+    public int get(int i)
+    {
+        
+    }
+    
+    
+}
---
+++ strutture/EsercizioArray/src/esercizioarray/EsercizioArray.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package esercizioarray;
+
+/**
+ *
+ * @author alex_2
+ */
+public class EsercizioArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
