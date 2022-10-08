a3285ec03b641822eea1833ec1f1f15426fb2c19
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2022-10-08 15:42:28 +0200

    Aggiunge la versione iniziale per progetto Frazione

---
+++ oop/ProgettoFrazione/src/Frazione.java
@@ -0,0 +1,9 @@
+public class Frazione {
+    private int numeratore;
+    private int denominatore;
+
+    public Frazione somma(Frazione frazione){
+
+    }
+
+}
---
+++ oop/ProgettoFrazione/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Frazione f, g, h;
+        f = new Frazione();
+        g = new Frazione();
+        h = f.somma(g);
+        System.out.println("Hello world!");
+    }
+}
