f342a5e23a6b0751eb134762aca6c875d64cb782
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2022-10-13 21:00:11 +0200

    Aggiunge i costruttori alla classe frazione

--- oop/ProgettoFrazione/src/Frazione.java
+++ oop/ProgettoFrazione/src/Frazione.java
@@ -2,8 +2,20 @@ public class Frazione {
     private int numeratore;
     private int denominatore;
 
-    public Frazione somma(Frazione frazione){
+    public Frazione(int numeratore, int denominatore){
+        this.numeratore = numeratore;
+        this.denominatore = denominatore;
+        if (this.denominatore == 0)
+            this.denominatore = 1;
+    }
 
+    public Frazione(String s){
+        //Qui andrebbe il codice di parsing della stringa, con eventuale
+        //controllo degli errori
+        this.numeratore = this.denominatore = 1;
+    }
+    public Frazione somma(Frazione frazione){
+        return null;
     }
 
 }
--- oop/ProgettoFrazione/src/Main.java
+++ oop/ProgettoFrazione/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         Frazione f, g, h;
-        f = new Frazione();
-        g = new Frazione();
-        h = f.somma(g);
+        f = new Frazione(1,2);
+        g = new Frazione(2,3);
+        h = new Frazione("1/23");
         System.out.println("Hello world!");
     }
 }
