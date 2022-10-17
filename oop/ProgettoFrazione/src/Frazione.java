c38015348eeed32d78ff388271f2747b2116d9b8
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2022-10-17 12:15:20 +0200

    Aggiunge il metodo toString e i metodi getter/setter

--- oop/ProgettoFrazione/src/Frazione.java
+++ oop/ProgettoFrazione/src/Frazione.java
@@ -8,12 +8,43 @@ public class Frazione {
         if (this.denominatore == 0)
             this.denominatore = 1;
     }
+    //Costruttore di copia perchè come paramtero ha un oggetto dello stesso tipo
+    public Frazione(Frazione f){
+        this.numeratore = f.numeratore;
+        this.denominatore = f.denominatore;
+    }
 
     public Frazione(String s){
         //Qui andrebbe il codice di parsing della stringa, con eventuale
         //controllo degli errori
         this.numeratore = this.denominatore = 1;
     }
+
+    @Override
+    public String toString() {
+        /*return "Frazione{" +
+                "numeratore=" + numeratore +
+                ", denominatore=" + denominatore +
+                '}';*/
+        return this.numeratore + "/" + this.denominatore;
+    }
+
+    public int getNumeratore() {
+        return numeratore;
+    }
+
+    public void setNumeratore(int numeratore) {
+        this.numeratore = numeratore;
+    }
+
+    public int getDenominatore() {
+        return denominatore;
+    }
+
+    public void setDenominatore(int denominatore) {
+        this.denominatore = denominatore;
+    }
+
     public Frazione somma(Frazione frazione){
         return null;
     }
--- oop/ProgettoFrazione/src/Main.java
+++ oop/ProgettoFrazione/src/Main.java
@@ -4,6 +4,17 @@ public class Main {
         f = new Frazione(1,2);
         g = new Frazione(2,3);
         h = new Frazione("1/23");
-        System.out.println("Hello world!");
+        //Utilizzo del costruttore di copia
+        Frazione i = new Frazione(h);
+
+        //Esempio di utilizzo del metodo toString
+        //Se non definito il metodo ritorna una stringa della forma
+        //nomeClasse@indirizzo (non è proprio un indirizzo...)
+        System.out.println(g.toString());
+
+        System.out.println(f);
+
+        //Esempio di utilizzo dei metodi getter
+        int somma = f.getNumeratore() + g.getNumeratore();
     }
 }
