56f7bee0f901f8309c8040ac29dfd683ce94eeea
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2023-04-14 15:43:38 +0200

    Aggiunge un semplice esempio con gli hash

--- collezioni/esempio_set/src/Main.java
+++ collezioni/esempio_set/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.TreeSet;
 
@@ -15,19 +16,19 @@ public class Main {
         ts.add(13);
         ts.add(99);
         System.out.println(ts);
-        if (hs.contains(34)){
+        if (hs.contains(34)) {
             System.out.println("Il set contiene il 34");
         }
         hs.clear();
         for (int i = 0; i < MAX_ELEMENTI; i++) {
-            hs.add((int)(Math.random()*MAX_ELEMENTI));
+            hs.add((int) (Math.random() * MAX_ELEMENTI));
         }
         //Verifico che eventuali duplicati non sono stati inseriti
         System.out.println(hs.size());
 
         int trovati = 0;
         for (int i = 0; i < MAX_ELEMENTI; i++) {
-            int cercato = (int)(Math.random()*MAX_ELEMENTI);
+            int cercato = (int) (Math.random() * MAX_ELEMENTI);
             if (hs.contains(cercato))
                 trovati++;
         }
@@ -36,24 +37,31 @@ public class Main {
         //Stesso codice per il TreeSet
         ts.clear();
         for (int i = 0; i < MAX_ELEMENTI; i++) {
-            ts.add((int)(Math.random()*MAX_ELEMENTI));
+            ts.add((int) (Math.random() * MAX_ELEMENTI));
         }
         //Verifico che eventuali duplicati non sono stati inseriti
         System.out.println(ts.size());
 
         trovati = 0;
         for (int i = 0; i < MAX_ELEMENTI; i++) {
-            int cercato = (int)(Math.random()*MAX_ELEMENTI);
+            int cercato = (int) (Math.random() * MAX_ELEMENTI);
             if (ts.contains(cercato))
                 trovati++;
         }
         System.out.println(trovati);
         for (int i = 0; i < MAX_ELEMENTI * 6; i++) {
-            int rimosso = (int)(Math.random()*MAX_ELEMENTI);
+            int rimosso = (int) (Math.random() * MAX_ELEMENTI);
             ts.remove(rimosso);
         }
         System.out.println(ts.size());
         System.out.println(ts);
-
+        hs = new HashSet<>();
+        ts = new TreeSet<>();
+        for (int i = 0; i < 5; i++) {
+            ts.add((int)(Math.random()*100000));
+            hs.add((int)(Math.random()*100000));
+        }
+        System.out.println(ts);
+        System.out.println(hs);
     }
 }
