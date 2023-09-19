7f95b75e937a19727d9d5a7e684890db19587846
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2023-09-19 14:59:19 +0200

    Aggiorna il programma in modo che funga da modello per i programmi successivi

--- introduzione/SelectionSort/src/SelectionSort.java
+++ introduzione/SelectionSort/src/SelectionSort.java
@@ -1,9 +1,15 @@
 public class SelectionSort {
-    private static void SelectionSort(int []v){
+    /**
+     * Algoritmo che implementa il SelectionSort
+     * @param v vettore da ordinare
+     * @param inizio primo elemento del vettore da ordinare
+     * @param fine ultimo elemento del vettore da ordinare
+     */
+    private static void SelectionSort(int []v, int inizio, int fine){
         //Selection Sort
-        for (int i = 0; i < v.length-1; i++) {
+        for (int i = inizio; i < fine; i++) {
             int PosMin = i;
-            for (int j = i+1; j < v.length; j++) {
+            for (int j = i+1; j <= fine; j++) {
                 if(v[j]<v[PosMin]){
                     PosMin =j;
                 }
@@ -14,19 +20,36 @@ public class SelectionSort {
         }
     }
     public static void main (String [] strings){
-        int []v;
-        v = new int[10];
+        final int LUNGHEZZA = 100000; //equivalente di const in C++
+        final boolean DEBUG = false;
 
-        for (int i = 0; i < 10; i++) {
+        int []v;
+
+        v = new int[LUNGHEZZA];
+
+        for (int i = 0; i < LUNGHEZZA; i++) {
             v[i] = (int)(Math.random()*100);
         }
-        for (int i = 0; i < 10; i++) {
-            System.out.print(" " + v[i]);
+
+        if (DEBUG) {
+            for (int i = 0; i < LUNGHEZZA; i++) {
+                System.out.print(" " + v[i]);
+            }
+            System.out.println();
         }
-        System.out.println();
-        SelectionSort(v);
-        for (int i = 0; i < 10; i++) {
-            System.out.print(" " + v[i]);
-        }
+
+        long inizio = System.nanoTime();
+        //Algoritmo di cui sto misurando le prestazioni
+        SelectionSort(v, 0, LUNGHEZZA - 1);
+        long fine = System.nanoTime();
+        float durata = (fine - inizio) / 1000000;
+        System.out.println("Il tempo impiegato dal SelectionSort per ordinare "
+                + LUNGHEZZA + " elementi è di " + durata + " ms");
+
+
+        if(DEBUG)
+            for (int i = 0; i < LUNGHEZZA; i++) {
+                System.out.print(" " + v[i]);
+            }
     }
 }
