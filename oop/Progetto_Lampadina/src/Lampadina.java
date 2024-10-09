6d2e72794b04fcfe90461f3fe1f1c39ebbfdd50b
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2024-10-09 12:55:24 +0200

    Gestione dei vettori di tipi primitivi e di oggetti.

--- oop/Progetto_Lampadina/src/Lampadina.java
+++ oop/Progetto_Lampadina/src/Lampadina.java
@@ -1,36 +1,48 @@
 public class Lampadina {
     private String nome;
     private final float potenza;
-    private int luminosita;
+    private int intensita;
     private String colore;
-    private boolean acceso;
+    private boolean accesa;
 
     public Lampadina(float potenza){
         this.potenza = potenza;
-        this.luminosita = 50;
+        this.intensita = 50;
         this.colore = "Bianco";
-        this.acceso = false;
+        this.accesa = false;
         this.nome = "";
     }
 
     public Lampadina(Lampadina l){
         this.potenza = l.potenza;
-        this.luminosita = l.luminosita;
+        this.intensita = l.intensita;
         this.colore = l.colore;
-        this.acceso = l.acceso;
+        this.accesa = l.accesa;
         this.nome = l.nome;
     }
 
     public void accendi(){
-        this.acceso = true;
+        this.accesa = true;
     }
 
     public void spegni(){
-        this.acceso = false;
+        this.accesa = false;
     }
 
     public boolean isAcceso(){
-        return this.acceso;
+        return this.accesa;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return this.getNome() + " : " + this.potenza + "W";
+    }
 }
--- oop/Progetto_Lampadina/src/Main.java
+++ oop/Progetto_Lampadina/src/Main.java
@@ -1,9 +1,54 @@
 public class Main {
+    public static void inizializzaLampadine(Lampadina[] v){
+        for (int i = 0; i < v.length; i++) {
+            v[i] = new Lampadina(80);
+            v[i].setNome("Lampadina " + (i+1));
+        }
+    }
+
+    public static Lampadina[] tornaAccese(Lampadina[] v){
+        Lampadina[] accese;
+        int conta = 0;
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] != null && v[i].isAcceso()){
+                conta++;
+            }
+        }
+        accese = new Lampadina[conta];
+        int j = 0;
+        for (int i = 0; i < v.length; i++) {
+            if (v[i] != null && v[i].isAcceso()){
+                accese[j] = v[i];
+                j++;
+            }
+        }
+        return accese;
+    }
+
     public static void main(String[] args) {
         Lampadina s = new Lampadina(8.8f);
         Lampadina f = new Lampadina(s);
 
         System.out.println(s);
         System.out.println(f);
+
+        int[] v = new int[10];
+        for (int i = 0; i < v.length; i++) {
+            v[i] = i;
+        }
+
+        Lampadina[] vl;
+        vl = new Lampadina[10];
+        inizializzaLampadine(vl);
+        vl[1].accendi();
+        vl[5].accendi();
+        for (Lampadina l: vl) {
+            System.out.println(l);
+        }
+        Lampadina[] accese = tornaAccese(vl);
+        for (Lampadina l: accese) {
+            System.out.println(l);
+        }
+
     }
 }
