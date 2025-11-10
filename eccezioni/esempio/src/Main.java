79695f571d849bff2fb9fd867eb66ef4a84695a3
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2025-11-10 12:01:42 +0100

    Miglioria

--- eccezioni/esempio/src/Main.java
+++ eccezioni/esempio/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -6,9 +8,10 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
         int[] a = new int[10];
-        for (int i = 0; i <= 10; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+        for (int i = 0; i < n; i++) {
             try{
                 a[i] = i * 3;
                 System.out.println(a[i]);
