d128bc776ce516ff50fe7c8a05ffe9ff6a2fb984
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2026-09-21 10:59:14 +0200

    Esempi di creazione di oggetti

--- oop/progetto-lampadina/src/Main.java
+++ oop/progetto-lampadina/src/Main.java
@@ -1,7 +1,22 @@
 public class Main{
     public static void main(String[] args){
-        Lampadina l;
+        Lampadina l, n;
         l = new Lampadina();
         l.potenza = 30;
+        System.out.println(l.potenza);
+        n = l;
+        System.out.println(n.potenza);
+        l.potenza = 40;
+        System.out.println(n.potenza);
+        System.out.println(l.potenza);
+        l = new Lampadina();
+        l.potenza = 50;
+        System.out.println(l.potenza);
+        System.out.println(n.potenza);
+        n = l;
+        System.out.println(l.potenza);
+        System.out.println(n.potenza);
+
+
     }
 }
