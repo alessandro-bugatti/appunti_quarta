b110f9e5d844ff08e16beca15b5b2d17cfed0e37
Alessandro Bugatti <alessandro.bugatti@gmail.com>
2026-09-23 10:30:15 +0200

    Aggiorna il progetto lampadina

--- oop/progetto-lampadina/src/Lampadina.java
+++ oop/progetto-lampadina/src/Lampadina.java
@@ -1,7 +1,30 @@
 public class Lampadina {
-    int potenza;
-    String colore;
-    int intensita;
-    boolean accesa;
-    String nome;
+    private int potenza;
+    private String colore;
+    private int intensita;
+    private boolean accesa;
+    private String nome;
+
+    public Lampadina(int potenza){
+        this.potenza = potenza;
+        if (this.potenza < 1 || this.potenza > 100) {
+            this.potenza = 10;
+        }
+        this.colore = "bianco";
+        this.intensita = 100;
+        this.accesa = false;
+        this.nome = "";
+    }
+
+    public void accendi(){
+        this.accesa = true;
+
+    }
+
+    public void aumentaLuminosita(){
+        this.intensita +=10;
+        if (this.intensita > 100){
+            this.intensita = 100;
+        }
+    }
 }
--- oop/progetto-lampadina/src/Main.java
+++ oop/progetto-lampadina/src/Main.java
@@ -1,7 +1,8 @@
 public class Main{
     public static void main(String[] args){
         Lampadina l, n;
-        l = new Lampadina();
+        //Esempi di utilizzo per capire la differenza tra riferimento e oggetto
+        /*l = new Lampadina();
         l.potenza = 30;
         System.out.println(l.potenza);
         n = l;
@@ -15,7 +16,9 @@ public class Main{
         System.out.println(n.potenza);
         n = l;
         System.out.println(l.potenza);
-        System.out.println(n.potenza);
+        System.out.println(n.potenza);*/
+        l = new Lampadina(30);
+        System.out.println(l);
 
 
     }
